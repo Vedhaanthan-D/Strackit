0d243144d4fa19dc0e3a4931628e4a255ebe8899
PRAKATID <pravedha@1979>
2025-10-09 16:26:18 +0530

    added the cart pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 import { 
   Navbar,
@@ -10,42 +10,84 @@ import {
   Static2Section,
   Footer
 } from './Home/components';
+import CartSidebar from './Home/components/sections/CartSidebar';
+import ViewCart from './Home/components/sections/ViewCart';
 import './App.css';
 
 function App() {
+  // Cart sidebar state
+  const [isCartSidebarOpen, setIsCartSidebarOpen] = useState(false);
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  // Cart handlers
+  const handleCartClick = () => {
+    setIsCartSidebarOpen(true);
+  };
+
+  const handleCartClose = () => {
+    setIsCartSidebarOpen(false);
+  };
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <ErrorBoundary>
-          <BannerSlider />
-        </ErrorBoundary>
-        <StaticTextSection />
-        <ErrorBoundary>
-          <MasterCategory />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <NewArrivals 
-            title="NEW ARRIVALS" 
-            subtitle="Nemo enim ipsam voluptatem quia voluptas sit aspernatur"
-            limit={8}
-            sortNewest={true}
-          />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <Static2Section />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <NewArrivals 
-            title="YOU MIGHT LIKE" 
-            subtitle="Nemo enim ipsam voluptatem quia voluptas sit aspernatur" 
-            limit={8}
-            sortNewest={false}
-          />
-        </ErrorBoundary>
+        <Navbar 
+          cartCount={cartItemCount}
+          onCartClick={handleCartClick}
+        />
+        
+        <Routes>
+          {/* Home Page Route */}
+          <Route path="/" element={
+            <>
+              <ErrorBoundary>
+                <BannerSlider />
+              </ErrorBoundary>
+              <StaticTextSection />
+              <ErrorBoundary>
+                <MasterCategory />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <NewArrivals 
+                  title="NEW ARRIVALS" 
+                  subtitle="Nemo enim ipsam voluptatem quia voluptas sit aspernatur"
+                  limit={8}
+                  sortNewest={true}
+                />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <Static2Section />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <NewArrivals 
+                  title="YOU MIGHT LIKE" 
+                  subtitle="Nemo enim ipsam voluptatem quia voluptas sit aspernatur" 
+                  limit={8}
+                  sortNewest={false}
+                />
+              </ErrorBoundary>
+            </>
+          } />
+          
+          {/* Cart Page Route */}
+          <Route path="/cart" element={
+            <ErrorBoundary>
+              <ViewCart />
+            </ErrorBoundary>
+          } />
+        </Routes>
+        
         <ErrorBoundary>
           <Footer />
         </ErrorBoundary>
+        
+        {/* Cart Sidebar */}
+        <CartSidebar 
+          isOpen={isCartSidebarOpen}
+          onClose={handleCartClose}
+          cartItemCount={cartItemCount}
+          setCartItemCount={setCartItemCount}
+        />
       </div>
     </Router>
   );
